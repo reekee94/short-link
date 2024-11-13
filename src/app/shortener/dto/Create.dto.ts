914315850ab217c3953958813cf67cd36971eff4
reekee94 <reekee94@gmail.com>
2024-11-13 21:59:37 +0100

    fix: can save only valid urls with protocols

--- src/app/shortener/dto/Create.dto.ts
+++ src/app/shortener/dto/Create.dto.ts
@@ -1,8 +1,8 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, IsUrl} from "class-validator";
 
 export class createDto{
     @IsNotEmpty()
     @IsString()
+    @IsUrl({require_protocol: true})
     original: string;
-
 }
